63dbb40f6e3bdf1a85b51915fb3b68f7b5e98f4e
kahyeelow <yeelow76@gmail.com>
2017-11-23 09:10:00 +0800

    hh

---
+++ app/src/main/java/my/edu/tarc/login/CriticalActivity.java
@@ -0,0 +1,13 @@
+package my.edu.tarc.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CriticalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_critical);
+    }
+}
